cd62875cd9a1fa5d80b92ab80d06fc619dfeb8ac
rafael.alvarez <rafael.alvarez.berrios@gmail.com>
2015-02-25 14:10:12 -0400

    Initial python unittest folder structure
     Changes to be committed:
    	new file:   test_python/__init__.py
    	new file:   test_python/test_lib/__init__.py
    	new file:   test_python/test_lib/test_a2audio/__init__.py
    	new file:   test_python/test_lib/test_a2audio/test_model.py
    	new file:   test_python/test_lib/test_a2audio/test_rec.py
    	new file:   test_python/test_lib/test_a2pyutils/__init__.py
    	new file:   test_python/test_lib/test_indices/__init__.py
    	new file:   test_python/test_lib/test_soundscape/__init__.py
    	new file:   test_python/test_scripts/__init__.py
    	new file:   test_python/test_scripts/test_PatternMatching/__init__.py
    	new file:   test_python/test_scripts/test_Soundscapes/__init__.py

---
+++ test_python/test_lib/test_a2audio/test_model.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Test_model(unittest.TestCase):
+
+    def test_this3(self):
+        self.assertTrue(True)
+
+    def test_this4(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_python/test_lib/test_a2audio/test_rec.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Test_rec(unittest.TestCase):
+
+    def test_this(self):
+        self.assertTrue(True)
+
+    def test_this2(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
