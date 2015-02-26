95b5aa093f58484914e0cd242827534fc571e8a9
rafael.alvarez <rafael.alvarez.berrios@gmail.com>
2015-02-26 14:41:25 -0400

    Libs json
    Update test_enviroment.py
    
     Changes to be committed:
    	new file:   test_python/data/libs.json
    	modified:   test_python/test_enviroment.py

--- test_python/test_enviroment.py
+++ test_python/test_enviroment.py
@@ -1,15 +1,22 @@
 import unittest
 import imp
-
-libs = ["numpy","scikits","skimage","virtualenv","argparse","png","wsgiref","boto","MySQLdb","scipy","joblib","matplotlib","sklearn"]
+import json
 
 def load(lib):
-    return imp.find_module(lib)
+    path = None
+    try:
+        fp,path,desc = imp.find_module(lib)
+    except:
+        """do nothing"""
+    return path
     
 class Test_enviroment(unittest.TestCase):
     def test_libs_imports(self):
+        libs = None
+        with open('test_python/data/libs.json') as fd:
+            libs = json.load(fd)
         for i in libs:
-            self.assertIsNotNone(load(i))
+            self.assertIsNotNone(load(i),msg="Lib "+i+" is not installed in current enviroment")
 
 if __name__ == '__main__':
     unittest.main()
