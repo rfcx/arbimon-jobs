cebbc2dca6fe7ca8c70fd53ee9117713ff0f4904
rafael.alvarez <rafael.alvarez.berrios@gmail.com>
2015-03-05 13:52:01 -0400

    Bmpio.py tests using mocks
     Changes to be committed:
    	modified:   lib/a2pyutils/bmpio.py
    	modified:   test_python/test_lib/test_a2pyutils/test_bmpio.py
    	new file:   test_python/test_lib/test_a2pyutils/test_news.py

--- test_python/test_lib/test_a2pyutils/test_bmpio.py
+++ test_python/test_lib/test_a2pyutils/test_bmpio.py
@@ -1,4 +1,8 @@
 import unittest
+import mock
+from mock import mock_open
+from a2pyutils.bmpio import Writer
+import struct
 
 class Test_bmpio(unittest.TestCase):
 
@@ -11,7 +15,6 @@ class Test_bmpio(unittest.TestCase):
             
     def test_init(self):
         """Test bmpio.Writer init arguments"""
-        from a2pyutils.bmpio import Writer
         import a2pyutils.palette as pl
         self.assertIsInstance( Writer() , Writer,msg="a2pyutils.bmpio.Writer initiation was not succesful")
         self.assertIsInstance( Writer(10) , Writer,msg="a2pyutils.bmpio.Writer initiation was not succesful")
@@ -22,18 +25,19 @@ class Test_bmpio(unittest.TestCase):
     def test_write(self):
         """Test bmpio.Writer write function"""
         import a2pyutils.bmpio
-        import filecmp
         import a2pyutils.palette as pl
-        import os
-        W = a2pyutils.bmpio.Writer
+        W = Writer
         fileout = "/tmp/bmpio.test.writer.bmp"
         w = W(width=1, height=255, bitdepth=8, palette=pl.get_palette())
-        with file(fileout, "wb") as fout:
-            w.write(fout, [[i] for i in range(255)])
-        self.assertTrue(os.path.isfile(fileout),msg="a2pyutils.bmpio.Writer failed to write bmp file")
-        self.assertTrue(filecmp.cmp(fileout,"test_python/data/bmpio.test.writer.bmp"),msg="a2pyutils.bmpio.Writer written bmp file is corrupt")
-        if(os.path.isfile(fileout )):
-            os.remove(fileout)
-        
+        m = mock_open()
+        with mock.patch('__builtin__.open', m, create=True):
+            with open(fileout, 'w') as fout:
+                w.write(fout, [[i] for i in range(255)])
+            self.assertTrue(m.called,msg="bmpio.Writer failed to call open method")
+            self.assertIsNone(m.assert_called_once_with(fileout, 'w'),msg="bmpio.Writer failed to write file")
+            handle = m()
+            for ii in range(255):
+                self.assertIsNone(handle.write.assert_any_call(struct.pack('<B',ii)),msg="bmpio.Writer failed to write some values")
+
 if __name__ == '__main__':
     unittest.main()
