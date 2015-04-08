c4024c935f6a53429de0b2f0139d7ca7ab6fee11
g-i-o- <aleph.omega@gmail.com>
2015-04-08 10:59:47 -0400

    added normalization to soundscape image in set_visual_scale

--- scripts/Soundscapes/set_visual_scale.py
+++ scripts/Soundscapes/set_visual_scale.py
@@ -5,15 +5,18 @@ import a2pyutils.palette
 from soundscape.set_visual_scale_lib import run
 
 USAGE = """
-{prog} soundscape_id max_visual_scale [palette_id]
+{prog} soundscape_id max_visual_scale [palette_id] [normalized]
     soundscape_id - id of the soundscape whose image to edit
     max_visual_scale - clip range maximum (if '-', then it is
                        computed automatically)
     palette_id - index of the gradient palette to use
                 (defined in a2pyutils.palette)
+    normalized - wether to normalize the the image by the
+                number of recordings in analized per column.
 """.format(
     prog=sys.argv[0]
 )
+
 
 def main(argv):
     if len(argv) < 3:
@@ -25,7 +28,8 @@ def main(argv):
         paletteId = (
             (int(argv[3]) if len(argv) > 3 else 0) %
             len(a2pyutils.palette.palette))
-        run(soundscapeId,clipMax,paletteId)
+        normalized = int(argv[4] if len(argv) > 4 else 0) != 0
+        run(soundscapeId, clipMax, paletteId, normalized)
         print 'end'
 
 if __name__ == '__main__':
