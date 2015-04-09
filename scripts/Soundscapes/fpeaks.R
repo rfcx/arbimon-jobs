b9625007c46a50293c574562f14afc019891447e
rafael.alvarez <rafael.alvarez.berrios@gmail.com>
2015-04-09 13:09:40 -0400

    fpeaks.R returns json array with [ {"f":feqsValue, "a":ampValue},... ]
     Changes to be committed:
    	new file:   scripts/Soundscapes/folder2Soundscape.py
    	modified:   scripts/Soundscapes/fpeaks.R
    	modified:   scripts/Soundscapes/playlist2soundscape.py

--- scripts/Soundscapes/fpeaks.R
+++ scripts/Soundscapes/fpeaks.R
@@ -1,72 +1,107 @@
 args = commandArgs(TRUE)
 suppressMessages(suppressWarnings(library(seewave)))
 suppressMessages(suppressWarnings(library(tuneR)))
-archivo<- readWave(args[1])
-AmplPeaks = c()
-if(length(archivo@left)>archivo@samp.rate)# at least one second of audio
+work_fpeaks = function(argss)
 {
-    bin_size = as.numeric(args[3])
-    picos = c()
-    spec = c()
-    srate = archivo@samp.rate
-    n = floor((srate)/bin_size) # search for the next power of two
-    n = n - 1
-    n = bitwOr(n,bitwShiftR(n, 1) )
-    n = bitwOr(n,bitwShiftR(n, 2)  )
-    n = bitwOr(n,bitwShiftR(n, 4) )
-    n = bitwOr(n,bitwShiftR(n, 8) )
-    n = bitwOr(n,bitwShiftR(n, 16) )
-    windowsize = n + 1
+    archivo = FALSE
     tryCatch(
         {
-            spec <- meanspec(archivo, f=srate, plot=FALSE,wl=windowsize)
+           archivo<- readWave(argss[1])
         }
         ,
         error = function(e)
         {
-            cat ('err1')
+            return('err0')
             quit()
-        }
+        }   
     );
-    epsilonValue = 0.00001
-    if(as.numeric(args[2]) > 0.00001)
+    AmplPeaks = c()
+    if(class(archivo) == 'Wave' || class(archivo) == 'WaveMC')
     {
-        epsilonValue = as.numeric(args[2])
-    }
-     
-    tryCatch(
+        data = c()
+        if(class(archivo) == 'Wave')
         {
-            #,amp=c(0.01,0.01)
-            picos<-fpeaks(spec,freq=as.numeric(args[4]),plot=FALSE,threshold=epsilonValue)
+            data = archivo@left
         }
-        ,
-        error = function(e)
+        else
         {
-            cat ('err2')
-            quit()
+            data = archivo@.Data
         }
-    );
-    
-    if( is.na(picos) || length(picos) < 1)
-    {
-       cat('err3')
-    }
-    else
-    {
-       picos[is.na(picos)]<-0
-       p<-dim(picos)
-       if (p[1]>=1)
-       {
-           pico<-data.frame(picos)
-           AmplPeaks<-lapply(1:length(pico[,1]),
-                               function (ii)
-                               {
-                                   cat(pico[ii,1])
-                                   cat(',')
-                               }
-                           )
-       }else cat ('err4')
-    }      
+        if(length(data)>archivo@samp.rate)# at least one second of audio
+        {
+            bin_size = as.numeric(argss[3])
+            picos = c()
+            spec = c()
+            srate = archivo@samp.rate
+            n = floor((srate)/bin_size) # search for the next power of two
+            n = n - 1
+            n = bitwOr(n,bitwShiftR(n, 1) )
+            n = bitwOr(n,bitwShiftR(n, 2)  )
+            n = bitwOr(n,bitwShiftR(n, 4) )
+            n = bitwOr(n,bitwShiftR(n, 8) )
+            n = bitwOr(n,bitwShiftR(n, 16) )
+            windowsize = n + 1
+            tryCatch(
+                {
+                    spec <- meanspec(archivo, f=srate, plot=FALSE,wl=windowsize)
+                }
+                ,
+                error = function(e)
+                {
+                    return('err1')
+                    quit()
+                }
+            );
+            epsilonValue = 0.00001
+            if(as.numeric(argss[2]) > 0.00001)
+            {
+                epsilonValue = as.numeric(argss[2])
+            }
+             
+            tryCatch(
+                {
+                    #,amp=c(0.01,0.01)
+                    picos<-fpeaks(spec,freq=as.numeric(argss[4]),plot=FALSE)#,threshold=epsilonValue)
+                }
+                ,
+                error = function(e)
+                {
+                    return('err2')
+                    quit()
+                }
+            );
+            if(is.null(picos))
+            {
+                return('[]')
+            }
+            if( is.na(picos) || length(picos[,1]) < 1)
+            {
+               return('[]')
+            }
+            else
+            {
+               picos[is.na(picos)]<-0
+               p<-dim(picos)
+               retStr = ''
+               if (p[1]>=1)
+               {
+                   pico<-data.frame(picos)
+                   retStr = paste( "{\"f\":", pico[1,1], ",\"a\":" ,pico[1,2]   , "}" ,sep="" )
+                   ii = 2
+                   while(ii <=length(pico[,1]))
+                   {
+                        retStr =paste(retStr, paste( "{\"f\":", pico[ii,1], ",\"a\":" ,pico[ii,2]   , "}" ,sep="" ) ,sep="," )
+                        ii = ii + 1
+                   }
 
-}else cat ('err5')
-
+                   return(paste("[",retStr,"]"))
+               }else return ('[]')
+            }      
+        
+        }else return('err5')
+    }else return('err6')
+}
+if(length(args) >=4)
+{
+    cat(work_fpeaks(args))
+}
