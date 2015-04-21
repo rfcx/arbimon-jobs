ce2d86b082b3e67358515ec1f7633f29385ebc3e
rafael.alvarez <rafael.alvarez.berrios@gmail.com>
2015-04-21 14:03:05 -0400

    Random species generator ver1
     Changes to be committed:
    	new file:   experimental/amb.wav
    	modified:   experimental/gen_random_species.py
    	modified:   experimental/test_syllable.py

--- experimental/gen_random_species.py
+++ experimental/gen_random_species.py
@@ -2,6 +2,8 @@ from pylab import *
 import numpy as np
 import math
 import random
+from scipy import interpolate
+import scipy
 
 syl_types = { 'spline' :{} }
 spline_types = {'overlaping':{},'nonoverlaping':{}}
@@ -10,29 +12,66 @@ class Spline(object):
     def __init__(self,xsize,ysize):
         self.xsize = xsize
         self.ysize = ysize
+        self.sig = None
         
     def gen_random_spline_signature(self,spline_type = 'nonoverlaping'):
-        num_of_points = 3
+        num_of_points = random.randint(3,6)
         self.spline_type = spline_type
         if spline_type not in spline_types:
             self.spline_type = 'nonoverlaping'
-        
+        sig = {}
         if spline_type == 'nonoverlaping':
             for i in range(num_of_points):
-                print "point",i
+                x=random.randint(0,self.xsize)
+                y=random.randint(0,self.ysize)
+                sig[i]= {'x':x,'y':y}
         elif spline_type == 'overlaping':
             print 'not yet implemented'
-            
-    def gen_spline(self):
-        pass
+        self.sig = sig 
     
-    def gen_random_point(self,xmi,xma,ymi,yma):
-        pass
-        #return randint(xmi,xma),randint(ymi,yma)
+    def test_sig(self):
+        x = []
+        y = []
+        for s in self.sig:
+            xx,yy = self.gen_random_point(self.sig[s]['x'],self.sig[s]['y'])
+            x.append(xx)
+            y.append(yy)
+        try:
+            tck,u = interpolate.splprep([x,y], s=0)
+            return True
+        except:
+            return False
+                
+    def gen_spline(self):
+        x = []
+        y = []
+        for s in self.sig:
+            xx,yy = self.gen_random_point(self.sig[s]['x'],self.sig[s]['y'])
+            x.append(xx)
+            y.append(yy)
+            
+        tck,u = interpolate.splprep([x,y], s=0)
+        
+        unew = np.arange(0, 1.01, 0.01)
+        out = interpolate.splev(unew, tck)
+        return out[0], out[1]
+        
+    def gen_random_point(self,x,y,tol=2):
+        xx=randint(x-tol,x+tol)
+        yy=randint(y-tol,y+tol)
+        if xx < 0:
+            xx = 0
+        if yy <0:
+            yy = 0
+        if xx >= self.xsize:
+            xx = self.xsize - 1
+        if yy >= self.ysize:
+            yy = self.ysize - 1
+        return xx,yy
     
 class Species(object):
     
-    def __init__(self,syllables=1,syl_type='spline',f_min = 2000 ,f_max = 4000,dur = .5 ,sample_rate = 44100,y_size = 512, noisy = True):
+    def __init__(self,syllables=1,syl_type='spline',f_min = 3000 ,f_max = 4000,dur = .5 ,sample_rate = 44100,y_size = 256, noisy = False):
         self.syl_type = syl_type
         if syl_type not in syl_types:
             self.syl_type='spline'
@@ -42,23 +81,147 @@ class Species(object):
         self.sample_rate = sample_rate
         self.y_size = y_size
         self.noisy = noisy
-        self.x_size = int(math.ceil(self.dur*self.sample_rate))
+        self.x_size_samples = int(math.ceil(self.dur*self.sample_rate))
+        self.x_size = self.x_size_samples/y_size
         self.syllables = syllables
         if noisy:
             self.matrix = np.random.rand(self.y_size,self.x_size)
         else:
             self.matrix = np.zeros(shape=(self.y_size,self.x_size))
+        
+        self.padding = 0.01 #secs
+        self.padding = int(math.ceil(self.padding*self.sample_rate))
+        self.splineX = self.x_size #- self.padding*2
+        self.maxFreq = float(self.sample_rate)/2.0
+        self.binFreq = float(self.maxFreq)/float(y_size)
+        f = float(self.maxFreq)
+        self.high_freq_index = y_size-1
+        while float(f) > float(f_max):
+            f = f - self.binFreq
+            self.high_freq_index = self.high_freq_index - 1
+        self.low_freq_index = self.high_freq_index
+        while float(f) > float(f_min):
+            f = f - self.binFreq
+            self.low_freq_index = self.low_freq_index - 1
+        
+        self.splineY = (self.high_freq_index-self.low_freq_index)
+        self.sp = Spline(self.splineX,self.splineY)
 
+        self.sp.gen_random_spline_signature()
+        while not self.sp.test_sig():
+            self.sp.gen_random_spline_signature()
+            
     def gen_random_individual(self):
-        pass
+        ind = self.matrix/2
+        v = 0.01
+        for i in range(ind.shape[0]):
+            ind[i,] = ind[i,] - v
+            v = v + 0.005
+        mm = np.mean(np.mean(ind))
+        
+        xx,yy = self.sp.gen_spline()
+        for i in range(len(xx)):
+            try:
+                ind[self.low_freq_index+int(yy[i]),int(xx[i])] = 0.95 
+            except:
+                """ """
+            try:
+                ind[self.low_freq_index+int(yy[i]),int(xx[i])+1] = 0.95 
+            except:
+                """ """
+            try:
+                ind[self.low_freq_index+int(yy[i]),int(xx[i])-1] = 0.95 
+            except:
+                """ """
+            try:
+                ind[self.low_freq_index+int(yy[i]+1),int(xx[i])] = 0.95 
+            except:
+                """ """
+            try:
+                ind[self.low_freq_index+int(yy[i]-1),int(xx[i])] = 0.95 
+            except:
+                """ """
+        return np.flipud(ind)
     
-    def gen_species_group(self):
-        pass
-    def save_species_group(self):
-        pass
-    def load_species_group(self):
-        pass
-
+    def gen_recording(self,calls=10,dur=10):
+        x_size_samples = int(math.ceil(dur*self.sample_rate))
+        x_size = x_size_samples/self.y_size
+        if self.noisy:
+            ind = np.random.rand(self.y_size,x_size)
+        else:
+            ind = np.zeros(shape=(self.y_size,x_size))
+        mm = np.mean(np.mean(ind))
+        sylDur = self.dur
+        remainingDur = dur
+        spos = 0
+        epos = self.x_size*2
+        cCount = 0
+        poss = []
+        for i in range(calls):
+            spos = random.randint(spos,epos)
+            epos = random.randint(max(spos,epos),epos)
+            xx,yy = self.sp.gen_spline()
+            hasSpline = False
+            for i in range(len(xx)):
+                try:
+                    ind[self.low_freq_index+int(yy[i]),spos+int(xx[i])] = 0.95
+                    hasSpline = True
+                except:
+                    """ """
+                try:
+                    ind[self.low_freq_index+int(yy[i]),spos+int(xx[i])+1] = 0.95
+                    hasSpline = True
+                except:
+                    """ """
+                try:
+                    ind[self.low_freq_index+int(yy[i]),spos+int(xx[i])-1] = 0.95
+                    hasSpline = True
+                except:
+                    """ """
+                try:
+                    ind[self.low_freq_index+int(yy[i]+1),spos+int(xx[i])] = 0.95
+                    hasSpline = True
+                except:
+                    """ """
+                try:
+                    ind[self.low_freq_index+int(yy[i]-1),spos+int(xx[i])] = 0.95
+                    hasSpline = True
+                except:
+                    """ """        
+                try:
+                    ind[self.low_freq_index+int(yy[i]),spos+int(xx[i])+2] = 0.95
+                    hasSpline = True
+                except:
+                    """ """
+                try:
+                    ind[self.low_freq_index+int(yy[i]),spos+int(xx[i])-2] = 0.95
+                    hasSpline = True
+                except:
+                    """ """
+                try:
+                    ind[self.low_freq_index+int(yy[i]+2),spos+int(xx[i])] = 0.95
+                    hasSpline = True
+                except:
+                    """ """
+                try:
+                    ind[self.low_freq_index+int(yy[i]-2),spos+int(xx[i])] = 0.95
+                    hasSpline = True
+                except:
+                    """ """
+            j = random.randint(int(float(x_size)/float(calls)),int(float(x_size*2)/float(calls)))
+            if hasSpline:
+                cCount =  cCount + 1
+                poss.append(spos+int(xx[i]))
+            spos = spos + j
+            epos = epos + j
+        return np.flipud(ind),cCount,poss
     
-    def view(self):
-        pass
+    def get_audio(self, spec ):
+       cols = spec.shape[1]
+       x = scipy.zeros(cols*spec.shape[0])
+       j = 0
+       for i in range(cols):
+           x[j:(j+spec.shape[0])] = scipy.real(scipy.ifft(spec[:,i]))
+           j = j + spec.shape[0]
+       return x       
+    
--- experimental/test_syllable.py
+++ experimental/test_syllable.py
@@ -1,5 +1,11 @@
 import unittest
 from mock import MagicMock
+from pylab import *
+import scipy.io.wavfile
+import numpy
+import subprocess
+import os
+import random
 
 class Test_syllable(unittest.TestCase):
 
@@ -10,13 +16,45 @@ class Test_syllable(unittest.TestCase):
             del sys.modules['gen_random_species']
         from gen_random_species import Species
         
-        sp = Species()
+        sp = Species(syllables=1,syl_type='spline',f_min = 3000 ,f_max = 5000,dur = .5 ,sample_rate = 44100,y_size = 256, noisy = False)
+        spec,cc,pos = sp.gen_recording(calls=15,dur=10)
+        print cc
+        au = sp.get_audio(spec)
+        spec2,a,b=mlab.specgram(au,NFFT=512,Fs=44100,noverlap=256)
+        imshow(10*log10(np.flipud(spec2[1:,])))
+        show()
         
+        response = raw_input("Do you like it? (y) ")
+        recCount = 30
+        #response = 'n'
+        del au
+        if response == 'y':
+            f = open('/home/rafa/Desktop/rec-stats.csv','w')
+            for i in range(recCount):
+                rr,count,pos = sp.gen_recording(calls=random.randint(5,15),dur=10)
+                au = sp.get_audio(rr)
+                scipy.io.wavfile.write( '/tmp/t.wav', 44100 , au)
+                del au
 
-        print dir(sp)
-
-        #print dir(syl)
-       
+                #mix ambient noise
+                proc = subprocess.Popen([
+                   '/usr/bin/sox', '-m',
+                   '/tmp/t.wav' , 'amb.wav' , '/tmp/t2.wav'
+                ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = proc.communicate()
+                sstr = '0'+str(i)
+                if int(i) >= 10:
+                    sstr = str(i)
+                proc = subprocess.Popen([
+                   '/usr/bin/sox',
+                   '/tmp/t2.wav' , '/home/rafa/Desktop/rec-2010-12-14_00-'+str(sstr)+'.wav' 
+                ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = proc.communicate()
+                os.remove('/tmp/t.wav')
+                os.remove('/tmp/t2.wav')
+                f.write( '/home/rafa/Desktop/rec-2010-12-14_00-'+str(sstr)+'.wav'+","+str(count)+","+','.join( [str(i) for i in pos])+'\n')
+            f.close()    
+                
 if __name__ == '__main__':
     unittest.main()
 
