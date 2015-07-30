3fbfb99ed3b9cfa4da4c3515e8e1ddafe32b0a0a
rafael-alvarez <rafael.alvarez.berrios@gmail.com>
2015-07-30 12:28:58 -0400

    Variables importance analysis script updated.
     Changes to be committed:
    	modified:   scripts/analisys/importances.py

--- scripts/analisys/importances.py
+++ scripts/analisys/importances.py
@@ -2,6 +2,49 @@ import sys
 import json
 import csv
 
+varssNames = ['mean',
+'(max-min)',
+'max',
+'min',
+'std',
+'median',
+'skew',
+'kurtosis',
+'moment(1) mean',
+'moment(2) variance',
+'moment(3) skweness',
+'moment(4) kurtosis',
+'moment(5) hyper skweness',  #high value indicates movement in the tails , while a low value indicates more action in the center
+'moment(6) hyper kurtosis',
+'moment(7)',
+'moment(8)',
+'moment(9)',
+'moment(10)',
+'cfs[0]',
+'cfs[1]',
+'cfs[2]',
+'cfs[3]',
+'cfs[4]',
+'cfs[5]',
+'hist[0]',
+'hist[1]',
+'hist[2]',
+'hist[3]',
+'hist[4]',
+'hist[5]',
+'mean(xf)',
+'max(xf)-min(xf)',
+'max(xf)',
+'min(xf)',
+'std(xf)',
+'median(xf)',
+'skew(xf)',
+'kurtosis(xf)',
+'acf[0]',
+'acf[1]',
+'acf[2]']
+
+
 filen = None
 if len(sys.argv) > 1:
     filen = str(sys.argv[1])
@@ -14,7 +57,18 @@ with open(filen, 'rb') as csvfile:
      spamreader = csv.reader(csvfile)
      for row in spamreader:
          importance_data.append([float(i) for i in row])
-    
-for row in importance_data:
-    print [i[0] for i in reversed(sorted(enumerate(row), key=lambda x:x[1]))]
+first = True
+firstNvars = 5
+a = None
+#f = open('/home/rafa/Desktop/allvarss.csv','a')
 
+for row in importance_data:
+    varss =  [i[0] for i in reversed(sorted(enumerate(row), key=lambda x:x[1]))]
+    #f.write(','.join(str(x) for x in row)+"\n")
+    if first:
+        a = varss[0:firstNvars]
+        first = False
+    else:
+        a = list(set(a) & set(varss[0:firstNvars]))
+#f.close()
+print [varssNames[i] for i in a]
