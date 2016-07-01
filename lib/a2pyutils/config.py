a07a16f6b6792cbfcff95a4dda261b21e4d48ecd
g-i-o- <aleph.omega@gmail.com>
2016-07-01 13:04:31 -0400

    removing old config code and files

--- lib/a2pyutils/config.py
+++ lib/a2pyutils/config.py
@@ -31,43 +31,6 @@ class AbstractConfig(object):
         self.cache[cfg] = config
         return config
 
-    
-
-class Config(AbstractConfig):
-    scache = {}
-
-    def __init__(self, basepath=None):
-        super(Config, self).__init__()
-
-        self.basepath = basepath if basepath else "config"
-
-        self.load('aws')
-        self.load('db')
-        self.load('path')
-
-    @classmethod
-    def for_path(cls, basepath):
-        if basepath not in cls.scache:
-            cls.scache[basepath] = cls(basepath)
-        return cls.scache[basepath]
-
-
-    def load(self, cfg):
-        cfgbasepath = os.path.join(self.basepath, cfg)
-
-        if os.path.isfile(cfgbasepath + '.local.json'):
-            cfgpath = cfgbasepath + '.local.json'
-        else:
-            cfgpath = cfgbasepath + '.json'
-
-        with open(cfgpath) as filedata:
-            config = json.load(filedata)
-
-        attr = cfg + "Config"
-        self.cache[cfg] = config
-        return config
-
-
 class EnvironmentConfig(AbstractConfig):
     scache = {}
 
