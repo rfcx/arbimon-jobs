46417792cbf6d966e92671c26227a13ea7478236
g-i-o- <aleph.omega@gmail.com>
2016-10-25 14:15:37 -0400

    adding taskrunner logic to lib

---
+++ lib/a2/job/taskrunner.py
@@ -0,0 +1,64 @@
+"""
+TaskRunner class
+"""
+
+import multiprocessing.pool
+import traceback
+
+class TaskRunner(object):
+    "Class for handling the running of tasks"
+    def __init__(self, config, max_concurrency):
+        self.max_concurrency = max_concurrency
+        self.config = config
+        self.tasks = []
+        self.pool = multiprocessing.pool.Pool(
+            max_concurrency
+        )
+        self.reporter_uri = None
+
+    def run(self, task, step):
+        
+        if len(self.tasks) >= self.max_concurrency:
+            raise AtMaximumConcurrencyError()
+
+        def resolve(arg):
+            print "resolved :: ", (task, step), arg
+            for i, t in reversed(list(enumerate(self.tasks))):
+                if t[0] == task and t[1] == step:
+                    del self.tasks[i]
+            
+        self.tasks.append((task, step))
+        self.pool.apply_async(execute_task, (task, step), callback=resolve)
+        
+        return {
+            "task": task,
+            "step": step
+        }
+        
+class TaskRunnerError(StandardError):
+    pass
+
+class AtMaximumConcurrencyError(TaskRunnerError):
+    pass    
+
+def execute_task(task, step):
+    try:
+        print "Executing ze task...", task, step
+        return True, sample_task()
+    except Exception:
+        return False, traceback.format_exc()
+
+def sample_task():
+    import time
+    import random
+    import os
+    
+    pid = os.getpid()
+    delay = int(random.uniform(1, 10))
+    a = random.uniform(1, 10)
+    b = random.uniform(1, 10)
+    
+    print "{} :: sleeping for {}s".format(pid, delay)
+    time.sleep(delay)
+    
+    return a + b
--- scripts/task_runner.py
+++ scripts/task_runner.py
@@ -6,8 +6,10 @@ Server for running job tasks.
 """
 
 import json
-import bottle
 import subprocess
+import bottle
+import a2.job.taskrunner
+import a2pyutils.config
 
 MAX_CONCURRENCY = 4
 PORT = 8760
@@ -43,37 +45,37 @@ class TaskRunnerBottle(bottle.Bottle):
     @register_provided_routes
     def __init__(self, max_concurrency):
         super(TaskRunnerBottle, self).__init__()
-        self.max_concurrency = max_concurrency
-        self.tasks = []
-        self.reporter_uri = None
+        self.task_runner = a2.job.taskrunner.TaskRunner(
+            a2pyutils.config.EnvironmentConfig(),
+            max_concurrency
+        )
 
     @provides_route('/health')
     def health(self):
         "returns health check"
-        bottle.response.content_type = 'application/json'
-        return json.dumps({"status": "ok"})
+        return {"status": "ok"}
 
     @provides_route('/status')
     def status(self):
         "returns status report"
-        bottle.response.content_type = 'application/json'
-        return json.dumps({
+        return {
             "status": "ok",
-            "reporter": self.reporter_uri,
-            "tasks": [t for t in self.tasks]
-        })
+            "reporter": self.task_runner.reporter_uri,
+            "tasks": [t for t in self.task_runner.tasks]
+        }
 
     @provides_route('/task/<task:int>', 'POST')
     @provides_route('/task/<task:int>/<step:int>', 'POST')
     def run_task(self, task, step=None):
         "runs a task"
+        try:
+            return self.task_runner.run(task, step)
+        except a2.job.taskrunner.AtMaximumConcurrencyError:
+            raise bottle.HTTPError(429, 'At Maximum Concurrency')
+
+    def default_error_handler(self, res):
         bottle.response.content_type = 'application/json'
-        return json.dumps({
-            "task": task,
-            "step": step
-        })
-
-
+        return json.dumps(dict(message=res.body, status="error"))
 
 if __name__ == '__main__':
     tbr = TaskRunnerBottle(MAX_CONCURRENCY)
