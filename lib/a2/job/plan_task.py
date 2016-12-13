878392d26a1beecf5e09963f22ffd1a09df8f52a
g-i-o- <aleph.omega@gmail.com>
2016-12-13 16:53:34 -0400

    adding job tasks, plus more planner methods

--- lib/a2/job/plan_task.py
+++ lib/a2/job/plan_task.py
@@ -5,20 +5,21 @@ import a2.runtime as runtime
 @runtime.tags.tag('task_type', 'job.plan')
 class PlanJobTask(tasks.Task):
     def run(self):
+        print "plan task running :-)"
         job = runtime.db.queryOne("""
             SELECT J.job_id as id, JTp.identifier as type
             FROM job_tasks JT
             JOIN jobs J ON JT.job_id = J.job_id
-            JOIN job_types JTp ON J.job_type_id = JTp.type_id
+            JOIN job_types JTp ON J.job_type_id = JTp.job_type_id
             WHERE JT.task_id = %s
         """, [self.taskId])
         
-        if not len(job):
+        if not job:
             raise StandardError("Task, job or job type not found in database.")
             
-        planner = runtime.tags.get('job_planner', job.type)
+        planner = runtime.tags.get('job_planner', job['type'])
         
         if not planner:
-            raise StandardError("Cannot plan job of type {}".format(job.type))
+            raise StandardError("Cannot plan job of type {}".format(job['type']))
         
-        planner(job.id).plan()
+        planner(job['id']).plan()
--- lib/a2/job/planner.py
+++ lib/a2/job/planner.py
@@ -1,4 +1,9 @@
 import json
+import a2.runtime as runtime
+
+JOB_PREPARE_WORKSPACE_TASK = 10
+JOB_END_TASK = 9
+SYNC_TASK = 8
 
 class JobPlanner(object):
     def __init__(self, jobId):
@@ -7,17 +12,43 @@ class JobPlanner(object):
     def plan(self):
         pass
     
-    def addTask(self, cursor, step, typeId, dependencies=None, args=None):
-        cursor.execute("""
+    def addTask(self, step, typeId, dependencies=None, args=None):
+        task_id = runtime.db.insert("""
             INSERT INTO job_tasks(job_id, step, type_id, dependency_counter, status, remark, timestamp, args)
             VALUES (%s, %s, %s, %s, 'waiting', NULL, NOW(), %s)
         """, [self.jobId, step, typeId, len(dependencies), json.dumps(args) if args else None])
-        task_id = cursor.lastrowid
-        
-        for dependency in dependencies:
-            cursor.execute("""
-                INSERT INTO job_task_dependencies(task_id, dependency_id, satisfied)
-                VALUES (%s, %s, 0)
-            """, [task_id, dependency])
+
+        self.addTaskDependencies([task_id], dependencies)
 
         return task_id
+    
+    def addSyncTask(self, step, dependencies):
+        return self.addTask(step, SYNC_TASK, dependencies)
+    
+    def addJobEndTask(self, step, dependencies):
+        return self.addTask(step, JOB_END_TASK, dependencies)
+    
+    def addPrepareWorkspaceTask(self, step, dependencies, folders=None):
+        return self.addTask(step, JOB_PREPARE_WORKSPACE_TASK, dependencies, folders)
+    
+    def addTaskDependency(self, task_id, dependency_id):
+        self.addTaskDependencies([task_id], [dependency_id])
+    
+    def addTaskDependencies(self, task_ids, dependency_ids):
+        for task_id in task_ids:
+            for dependency_id in dependency_ids:
+                runtime.db.insert("""
+                    INSERT INTO job_task_dependencies(task_id, dependency_id, satisfied)
+                    VALUES (%s, %s, 0)
+                """, [task_id, dependency_id])
+
+    def deleteStepsHigherThan(self, step):
+        runtime.db.execute("""
+            DELETE FROM job_tasks 
+            WHERE job_id = %s and step > %
+        """, [self.jobId, step])
+
+
+    def makeWorkspace(self, subdirs):
+        base_path = os.path.join(runtime.config.pathsConfig['efs_base'], 'jobs', self.jobId)
+        
