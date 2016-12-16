299747733229f16f27c63d0ba61ad7b76c452ebb
g-i-o- <aleph.omega@gmail.com>
2016-12-16 17:21:22 -0400

    adding a2.runtime.tmp module

---
+++ lib/a2/runtime/tmp.py
@@ -0,0 +1,39 @@
+"""
+Utility module for temp files.
+"""
+
+import os
+import tempfile as __tempfile
+
+class tempfile(object):
+    """
+    A tempfile tracker object.
+    provides usefull enter, close context handlers to automatically 
+    create and remove a tempfile using mkstemp() from the builtin tempfile module.
+    Cosntructor arguments are given to mkstemp() upon entering the context
+    and self is returned. The tempfile is removed (os.remove()) upon exiting the context.
+    The tempfile's file handle can be closed mid-way with the close_file() method.
+    """
+    def __init__(self, *args, **kwargs):
+        "Initialize the tracker object, arguments are given to tempfile.mkstemp()"
+        self.args = args
+        self.kwargs = kwargs
+        self.file=None
+        self.filename=None
+        
+    def close_file(self):
+        "Closes the tempfile's file handle"
+        self.file.close()
+        self.file = None
+        
+    def __enter__(self):
+        "Creates a tempfile upon entering the context"
+        self.file, self.filename = __tempfile.mkstemp(*self.args, **self.kwargs)
+        return self
+        
+    def __exit__(self, xtype, value, traceback):
+        "Removes the tempfile upon exiting the context"
+        if self.file:
+            self.file.close()
+        
+        os.remove(self.filename)
