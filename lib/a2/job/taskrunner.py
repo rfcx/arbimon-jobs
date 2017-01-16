0518452c58e98fcae459324af8ef8b2d5ae4bdfa
g-i-o- <aleph.omega@gmail.com>
2017-01-16 11:37:35 -0400

    pylinting

--- lib/a2/job/taskrunner.py
+++ lib/a2/job/taskrunner.py
@@ -1,6 +1,7 @@
 """
 TaskRunner class
 """
+# pylint: disable=W0703, R0903, C0103
 
 import multiprocessing
 import multiprocessing.pool
@@ -19,32 +20,36 @@ class TaskRunner(object):
         self.reporter_uri = None
 
     def run(self, task, callback=None):
-        
+        "Runs the given task in parallel, calling the callback afterwards"
         if len(self.tasks) >= self.max_concurrency:
             raise AtMaximumConcurrencyError()
 
         def resolve(arg):
+            "called when the tsk is resolved"
             print "resolved :: ", task, arg
             for i, t in reversed(list(enumerate(self.tasks))):
                 if t == task:
                     del self.tasks[i]
             if callback:
                 callback(arg)
-            
+
         self.tasks.append(task)
         self.pool.apply_async(execute_task, (task, ), callback=resolve)
-        
+
         return {
             "task": task
         }
-        
+
 class TaskRunnerError(StandardError):
+    "An error associated to the task runnner."
     pass
 
 class AtMaximumConcurrencyError(TaskRunnerError):
-    pass    
+    "The task runner is at maximum concurrency."
+    pass
 
 def execute_task(taskId):
+    "Executes the task of the given task id."
     try:
         print "Executing ze task...", taskId
         task = tasks.Task.fromTaskId(taskId)
@@ -63,16 +68,17 @@ def execute_task(taskId):
         return False, exc
 
 def sample_task():
+    "sample task."
     import time
     import random
     import os
-    
+
     pid = os.getpid()
     delay = int(random.uniform(1, 10))
     a = random.uniform(1, 10)
     b = random.uniform(1, 10)
-    
+
     print "{} :: sleeping for {}s".format(pid, delay)
     time.sleep(delay)
-    
+
     return a + b
