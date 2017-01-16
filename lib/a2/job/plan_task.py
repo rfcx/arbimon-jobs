d6321c0b9db9a34fb8a1282182daf79430f56daa
g-i-o- <aleph.omega@gmail.com>
2017-01-16 10:55:34 -0400

    added docstring +bugfixes

--- lib/a2/job/plan_task.py
+++ lib/a2/job/plan_task.py
@@ -1,9 +1,21 @@
+"""
+Job Planner task module.
+defines the job planner task, wich delegates the actual job planning to a
+tagged 'job.planner' of the given job type.
+"""
+
 import tasks
+
 import a2.runtime as runtime
 
 
 @runtime.tags.tag('task_type', 'job.plan')
 class PlanJobTask(tasks.Task):
+    """
+    Job Planner task.
+    Delegates the actual job planning to a
+    tagged 'job.planner' of the given job type.
+    """
     def run(self):
         print "plan task running :-)"
         job = runtime.db.queryOne("""
@@ -13,13 +25,13 @@ class PlanJobTask(tasks.Task):
             JOIN job_types JTp ON J.job_type_id = JTp.job_type_id
             WHERE JT.task_id = %s
         """, [self.taskId])
-        
+
         if not job:
             raise StandardError("Task, job or job type not found in database.")
-            
-        planner = runtime.tags.get('job_planner', job['type'])
-        
+
+        planner = runtime.tags.get('job.planner', job['type'])
+
         if not planner:
             raise StandardError("Cannot plan job of type {}".format(job['type']))
-        
-        planner(job['id']).plan()
+
+        planner(job['id'], self.taskId).plan()
