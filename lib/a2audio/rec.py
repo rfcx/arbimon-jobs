b111e51969cc8aa84cc825666878808c2b6ffbd0
jackgle <jglebien@gmail.com>
2021-03-31 21:49:04 -0500

    opus support fix

--- lib/a2audio/rec.py
+++ lib/a2audio/rec.py
@@ -207,6 +207,13 @@ class Rec:
         return enc
 
     def readAudioFromFile(self):
+
+        if self.filename.split('.')[-1]=='opus':
+            process = subprocess.Popen(['opusdec', self.localfilename, self.localfilename+'.wav'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = process.communicate()
+            self.localfilename = self.localfilename+'.wav'
+            self.logs.write('decoded opus file: '+str(self.filename))
+
         try:
             with contextlib.closing(Sndfile(self.localfilename)) as f:
                 if self.logs:
