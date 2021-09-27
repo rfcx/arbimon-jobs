beeda34f9ba3fdcab24b650bbcef4fff2d6f7834
Antony Harfield <antonyharfield@gmail.com>
2021-09-27 23:50:23 +0100

    Add R dependencies to setup scripts

---
+++ scripts/setup/020-r-packages.R
@@ -0,0 +1,6 @@
+#!/usr/bin/Rscript --vanilla
+install.packages("fftw", repos="http://cran.us.r-project.org")
+install.packages("https://cran.r-project.org/src/contrib/Archive/signal/signal_0.7-4.tar.gz", repos=NULL)
+install.packages("https://cran.r-project.org/src/contrib/Archive/tuneR/tuneR_1.2.1.tar.gz", repos=NULL)
+install.packages("https://cran.r-project.org/src/contrib/Archive/seewave/seewave_1.7.6.tar.gz", repos=NULL)
+install.packages("RUnit", repos="http://cran.us.r-project.org")
